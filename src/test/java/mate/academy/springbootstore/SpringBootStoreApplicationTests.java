2a6512ff52c5e73e5f2f036a1af80411357c5fa4
KLICKICE <ledmaskiight@gmail.com>
2025-07-27 17:16:38 +0300

    Initial project setup

---
+++ src/test/java/mate/academy/springbootstore/SpringBootStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package mate.academy.springbootstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
