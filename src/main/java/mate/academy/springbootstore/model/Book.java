0fa4dfd091913542415a27f67918aa9f4c264965
KLICKICE <ledmaskiight@gmail.com>
2025-07-29 19:54:53 +0300

    Fixed CI, replaced Double to BigDecimal, added server.servlet.context-path for the /api prefix

--- src/main/java/mate/academy/springbootstore/model/Book.java
+++ src/main/java/mate/academy/springbootstore/model/Book.java
@@ -1,13 +1,14 @@
 package mate.academy.springbootstore.model;
 
-import jakarta.persistence.*;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Positive;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
-
-import java.math.*;
+import java.math.BigDecimal;
 
 @Getter
 @Setter
