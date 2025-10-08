07fc9e5bd0c6ac44695ac876a3f63505e767dba0
KLICKICE <ledmaskiight@gmail.com>
2025-10-08 18:49:52 +0300

    Fixed Mapper and Dto

--- src/main/java/mate/academy/springbootstore/dto/category/CategoryDto.java
+++ src/main/java/mate/academy/springbootstore/dto/category/CategoryDto.java
@@ -1,11 +1,14 @@
 package mate.academy.springbootstore.dto.category;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class CategoryDto {
     private Long id;
     private String name;
     private String description;
 }
-
--- src/main/java/mate/academy/springbootstore/mapper/BookMapper.java
+++ src/main/java/mate/academy/springbootstore/mapper/BookMapper.java
@@ -1,17 +1,34 @@
 package mate.academy.springbootstore.mapper;
 
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 import mate.academy.springbootstore.config.MapConfig;
 import mate.academy.springbootstore.dto.book.BookDto;
 import mate.academy.springbootstore.dto.book.CreateBookRequestDto;
+import mate.academy.springbootstore.dto.category.CategoryDto;
 import mate.academy.springbootstore.model.Book;
+import mate.academy.springbootstore.model.Category;
 import org.mapstruct.Mapper;
 import org.mapstruct.MappingTarget;
 
 @Mapper(config = MapConfig.class)
 public interface BookMapper {
+
     BookDto toDto(Book book);
 
     Book toModel(CreateBookRequestDto requestDto);
 
     void updateBookFromDto(CreateBookRequestDto book, @MappingTarget Book entity);
+
+    default List<CategoryDto> mapCategories(Set<Category> categories) {
+        if (categories == null) {
+            return Collections.emptyList();
+        }
+        return categories.stream()
+                .map(c -> new CategoryDto(c.getId(), c.getName(), null))
+                .collect(Collectors.toList());
+    }
 }
+
